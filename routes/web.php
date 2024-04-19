43a6a619b8279e631ce1fa4bd71470148e48613a
ayunita putri <contoh@ayunita>
2024-04-19 13:21:57 +0700

    bab 5

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ProductController;
-
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,4 +26,4 @@ Route::resource('kategori',CategoryController::class)
 ->except('show','destroy','create');
 Route::resource('pelanggan',CustomerController::class)->except('destroy');
 Route::resource('produk',ProductController::class);
-
+Route::resource('pengguna',UserController::class)->except('destroy','create','show','update','edit',);
